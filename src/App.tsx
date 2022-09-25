efb88bee185cc6312e749dc2353a6ec3877b6501
derodero24 <k.itsh20524abc@gmail.com>
2022-09-25 22:12:12 +0900

    add OrbitControls

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { OrbitControls } from '@react-three/drei';
 import { Canvas } from '@react-three/fiber';
 
 import Box from './Box';
@@ -9,6 +10,8 @@ export default function App(): JSX.Element {
       <pointLight position={[10, 20, 10]} />
       <Box position={[-1.2, 0, 0]} />
       <Box position={[1.2, 0, 0]} />
+      <OrbitControls />
+      <gridHelper />
     </Canvas>
   );
 }
